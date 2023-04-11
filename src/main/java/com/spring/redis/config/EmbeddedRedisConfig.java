1b992bd742486eeeaed4c5aa8914cb7f7640a362
DongJu-Na <ehdeld123@naver.com>
2023-04-11 18:57:29 +0900

    내장 redis에서도 redis template 사용가능하도록 처리
    내장 redis에서도 redis template 사용가능하도록 처리

--- src/main/java/com/spring/redis/config/EmbeddedRedisConfig.java
+++ src/main/java/com/spring/redis/config/EmbeddedRedisConfig.java
@@ -1,13 +1,26 @@
 package com.spring.redis.config;
 
+import java.time.Duration;
 import java.util.Optional;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.TimeoutOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
@@ -42,7 +55,45 @@ public class EmbeddedRedisConfig implements InitializingBean , DisposableBean {
 	  redisServer = new RedisServer(port);
     redisServer.start();
     log.info("EmbeddedRedisServer Start > " + port);
-	} 
+	}
+	
+	 @Bean
+   public LettuceConnectionFactory redisConnectionFactory() {
+       RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+       redisConfiguration.setHostName("localhost");
+       redisConfiguration.setPort(port);
+
+       LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+               .clientResources(clientResources())
+               .clientOptions(ClientOptions.builder()
+                       .socketOptions(SocketOptions.builder()
+                               .connectTimeout(Duration.ofSeconds(10))
+                               .build())
+                       .timeoutOptions(TimeoutOptions.enabled(Duration.ofSeconds(10)))
+                       .build())
+               .build();
+
+       return new LettuceConnectionFactory(redisConfiguration, clientConfiguration);
+   }
+
+   @Bean
+   public RedisTemplate<?, ?> redisTemplate() {
+       RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+       redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+       redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+       redisTemplate.setKeySerializer(new StringRedisSerializer());
+       redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+       redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+       redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+       return redisTemplate;
+   }
+
+   @Bean
+   public ClientResources clientResources() {
+       return DefaultClientResources.create();
+   }
 	
 		
 }
