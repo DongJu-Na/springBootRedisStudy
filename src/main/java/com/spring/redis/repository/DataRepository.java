9bb36d2c2a0a110229e263a163b0f8a00d98a6cc
DongJu-Na <ehdeld123@naver.com>
2023-04-12 16:10:13 +0900

    Repository deprecated
    Repository deprecated

--- src/main/java/com/spring/redis/repository/DataRepository.java
+++ src/main/java/com/spring/redis/repository/DataRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.spring.redis.model.Data;
 
+/*
+ * @deprecated RedisTemplate 주로 사용 예정 그러므로 Repository 사용x 23.04.12
+ */
+@Deprecated
 @Repository
 public interface DataRepository extends CrudRepository<Data, String> {
 
